1663025b82d09e75aa9feb7fa7470932495c1ba1
Verónica Flores <veritocalla@gmail.com>
2021-09-24 18:03:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Create WebSocket connection.
+
+const socket = new WebSocket('ws://10.13.8.99:8000/ws/chat/elfolibre/');
+
+// Connection opened
+socket.onopen = () => {
+  console.log('is connected')
+};
+
+socket.onmessage = (message) => {
+  console.log(message);
+}
+
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+
   return (
     <div className="App">
       <header className="App-header">
